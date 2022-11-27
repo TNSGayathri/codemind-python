fa2532ddaea76bc84d739bac5f952b309fbf4509
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-11-27 13:26:20 +0530

    n=int(input()) l=list(map(int,input().split())) for i in range(len(l)):     c=1     for j in range(len(l)):         if(i!=j and l[i]==l[j]):             c+=1             l[j]="-1"     if(c>n/2):         print(l[i])         break

---
+++ Majority_Element.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+for i in range(len(l)):
+    c=1
+    for j in range(len(l)):
+        if(i!=j and l[i]==l[j]):
+            c+=1
+            l[j]="-1"
+    if(c>n/2):
+        print(l[i])
+        break
