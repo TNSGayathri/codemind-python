cd6d82889037a203f2a4da704ed6e18f10b9e428
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-11-27 18:48:56 +0530

    def ans(n):     f=0     for i in range(2,n//2+1):         if(n%i==0):             f=1             break     return f n=int(input()) c=1 for i in range(2,n+1):     if(n%i==0):         a=ans(i)         if(a==1):             c+=1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,22 +1,15 @@
-def res(n):
-    fact=0
-    d=0
+def ans(n):
+    f=0
     for i in range(2,n//2+1):
-        if(n==1):
-            fact=1
+        if(n%i==0):
+            f=1
             break
-        elif(n%i==0):
-            fact=1
-            break
-    if(fact==1):
-        d=1
-    return d
+    return f
 n=int(input())
 c=1
-for i in range(1,n+1):
+for i in range(2,n+1):
     if(n%i==0):
-        a=res(i)
+        a=ans(i)
         if(a==1):
             c+=1
-print(c)
-        
+print(c)          
