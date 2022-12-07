a27c7128009be2a4d5ffba54f1a5b02f2fe22609
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-12-07 05:34:11 +0530

    n=int(input()) res=0 while(n>0):     r=n%10     n=n//10     res+=r*r     if(n==0 and res>9):         n=res         res=0 if(res==1):     print("True") else:     print("False")

--- happy_number.py
+++ happy_number.py
@@ -2,12 +2,12 @@ n=int(input())
 res=0
 while(n>0):
     r=n%10
-    res+=r*r
     n=n//10
+    res+=r*r
     if(n==0 and res>9):
         n=res
         res=0
-if(res==1 ):
+if(res==1):
     print("True")
 else:
     print("False")
