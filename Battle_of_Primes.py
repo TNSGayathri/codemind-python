b519b8119bbd9af68e812c09b66f376d520c1a11
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2022-12-13 14:16:21 +0530

    def ans(n):     f=1     if(n==1):         f=0     for i in range(2,n//2+1):         if(n%i==0):             f=0             break     return f a=int(input()) b=int(input()) for i in range((a+b)+1,999999):     if(ans(i)==1):         print(i-(a+b))         break

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,15 +1,15 @@
 def ans(n):
-    f=0
+    f=1
     if(n==1):
-        f=1
+        f=0
     for i in range(2,n//2+1):
         if(n%i==0):
-            f=1
+            f=0
             break
     return f
-n=int(input())
-m=int(input())
-for i in range((n+m)+1,999999):
-    if(ans(i)==0):
-        print(abs((n+m)-i))
+a=int(input())
+b=int(input())
+for i in range((a+b)+1,999999):
+    if(ans(i)==1):
+        print(i-(a+b))
         break
