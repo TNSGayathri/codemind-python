d2359900dc7442b1c60c957fb700f5271dc778df
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2023-05-11 18:54:51 +0530

    def sub_array(l):     s=[]     m=[]     for i in range(len(l)+1):         for j in range(i+1,len(l)+1):             s=l[i:j]             m.append(s)     return m a=int(input()) b=int(input()) l=[i for i in range(a,b+1)] m=sub_array(l) c=0 for i in m:     if sum(i)%2!=0:         c+=1 print(c)

--- Distinct_subarray_.py
+++ Distinct_subarray_.py
@@ -1,13 +1,17 @@
+def sub_array(l):
+    s=[]
+    m=[]
+    for i in range(len(l)+1):
+        for j in range(i+1,len(l)+1):
+            s=l[i:j]
+            m.append(s)
+    return m
 a=int(input())
 b=int(input())
+l=[i for i in range(a,b+1)]
+m=sub_array(l)
 c=0
-l=[]
-for i in range(a,b+1):
-    if i %2==1:
+for i in m:
+    if sum(i)%2!=0:
         c+=1
-    l.append(i)
-for i in range(0,len(l)-1):
-    for j in range(i+1,len(l)):
-        if (l[i]%2==0 and l[j]%2==1) or (l[i]%2==1 and l[j]%2==0):
-            c+=1
 print(c)
