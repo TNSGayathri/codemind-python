25739a45732c83bc338ef35347238513b12f1617
TNSGayathri <96291654+TNSGayathri@users.noreply.github.com>
2023-07-04 11:28:45 +0530

    n=input().lower().split() m=input().lower().split() n="".join(n) m="".join(m) s=[] for i in n:     if i not in m  and i not in s:         s.append(i) for i in m:     if i not in n and i not in s:         s.append(i) print(len(s))

---
+++ Count_of_non_common_characters.py
@@ -0,0 +1,13 @@
+n=input().lower().split()
+m=input().lower().split()
+n="".join(n)
+m="".join(m)
+s=[]
+for i in n:
+    if i not in m  and i not in s:
+        s.append(i)
+for i in m:
+    if i not in n and i not in s:
+        s.append(i)
+print(len(s))
+
